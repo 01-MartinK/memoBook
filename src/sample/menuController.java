56ea8bd05807cb567626136bc95a52da5d072572
MK <thedeadgamer101@gmail.com>
2021-04-09 22:10:31 +0300

    Changed manual date entering into calendar date selection

--- src/sample/menuController.java
+++ src/sample/menuController.java
@@ -1,6 +1,11 @@
 package sample;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import sample.datamodel.Memo;
@@ -11,14 +16,17 @@ public class menuController {
     private TextField titelField;
 
     @FXML
-    private TextField dateField;
+    private DatePicker datePicker;
 
     @FXML
     private TextArea contentField;
 
     public Memo getNewMemo(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dateTime = datePicker.getValue();
+
+        String date = formatter.format(dateTime).toString();
         String titel = titelField.getText();
-        String date = dateField.getText();
         String content = contentField.getText();
 
         Memo newContact = new Memo(titel,date,content);
@@ -26,15 +34,22 @@ public class menuController {
     }
 
     public void editMemo(Memo selectedMemo) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate date = LocalDate.parse(selectedMemo.getKuupaev(), formatter);
+
         titelField.setText(selectedMemo.getTiitel());
-        dateField.setText(selectedMemo.getKuupaev());
+        datePicker.setValue(date);
         contentField.setText(selectedMemo.getSisu());
 
     }
 
     public void updateMemo(Memo selectedMemo) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dateTime = datePicker.getValue();
+
         selectedMemo.setTiitel(titelField.getText());
-        selectedMemo.setKuupaev(dateField.getText());
+        selectedMemo.setKuupaev(formatter.format(dateTime).toString());
         selectedMemo.setSisu(contentField.getText());
     }
 }
